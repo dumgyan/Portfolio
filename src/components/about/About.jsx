4f478bce158f9e4a30613b9aa77f771e12cee588
Rabbi Kojo Dumgyan <dumgyanr8@gmail.com>
2025-06-07 08:43:00 +0000

    updated CV

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -88,10 +88,10 @@ const About = () => {
               and bring your ideas to the forefront of technology.
             </p>
             <a
-              href="assets/resume.pdf" target="_blank" rel="noopener noreferrer"
+              href="assets/resume_old.pdf" target="_blank" rel="noopener noreferrer"
               className="inline-flex justify-center items-center py-2.5 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
             >
-              Download CV
+              Previous CV
               <svg
                 className="w-3.5 h-3.5 ms-2 rtl:rotate-180"
                 aria-hidden="true"
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -46,9 +46,9 @@ const Home = () => {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <button className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+              <a href="assets/resume_new.pdf"> <button className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
                 Get Resume
-              </button>
+              </button></a>
             </a>
           </div>
           <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
